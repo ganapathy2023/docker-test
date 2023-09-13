adbc77c610cd279f52e656bc4420e64bea6a8a3f
ganapathy2023 <144091878+ganapathy2023@users.noreply.github.com>
2023-09-13 12:27:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const request = require("request");
 const express = require('express');
 
 // Constants
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8003;
 const HOST = process.env.HOST || 'localhost';
 
 // App
 const app = express();
 
 app.get('/', (req, res) => {
-  return res.send(`Hello World`);
+  return res.send(`Hello Nemilink`);
 });
 
 app.listen(PORT);
